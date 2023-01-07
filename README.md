9a04601976a89960f598b5de17231685ff924699
Xonin <xonindev@gmail.com>
2023-01-07 06:03:29 +0000

    update code and readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A repository for my c++ homeworks
 
 
 ## Technologies used
-![C++](https://img.shields.io/badge)
+![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)
 
 ## Credits
 This project was made by [Xonin](https://github.com/xonin-hush)
--- home-work2/hw3/hmw3.cpp
+++ home-work2/hw3/hmw3.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 using namespace std;
 int main()
-{ int a ,b ,c ,d ,e ,av ,sum ,checked;
-cout<< "enter your A ";
-cin >>a;
-cout<< "enter your B ";
-cin >>b;
-cout<< "enter your C ";
-cin >> c;
-cout<< "enter your D ";
-cin >> d;
-cout<< "enter your E ";
-cin >> e;
-(sum=a+b+c+d+e);
-(av=sum/5);
-(checked=av/10);
-cout <<"your average is "; cout <<av <<endl;
-switch (checked)
-{   case 10 : cout << "excellent"; break;
-    case 9 : cout << "excellent"; break;
-    case 8 : cout << "very good"; break;
-    case 7 : cout << "good"; break;
-    case 6 : cout << "medium"; break;
-    case 5 : cout << "pass"; break;
-    case 4 : cout << "fail"; break;
-    case 3 : cout << "fail"; break;
-    case 2 : cout << "fail"; break;
-    case 1 : cout << "fail"; break;
-    case 0 : cout << "fail"; break;
-    default : cout<<  "error"; break;
-}
-
+{
+    int a, b, c, d, e, av, sum, checked;
+    cout << "enter your A ";
+    cin >> a;
+    cout << "enter your B ";
+    cin >> b;
+    cout << "enter your C ";
+    cin >> c;
+    cout << "enter your D ";
+    cin >> d;
+    cout << "enter your E ";
+    cin >> e;
+    sum = a + b + c + d + e;
+    av = sum / 5;
+    checked = av / 10;
+    cout << "your average is ";
+    cout << av << endl;
+    switch (checked)
+    {
+    case 10:
+    case 9:
+        cout << "excellent";
+        break;
+    case 8:
+        cout << "very good";
+        break;
+    case 7:
+        cout << "good";
+        break;
+    case 6:
+        cout << "medium";
+        break;
+    case 5:
+        cout << "pass";
+        break;
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        cout << "fail";
+        break;
+    default:
+        cout << "error";
+    }
 }
